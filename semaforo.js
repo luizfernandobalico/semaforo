5cb8582cb95576845b4a93b833a4f3277008c23c
Luiz Fernando <luizfernandobalico@gmail.com>
2025-01-28 16:35:48 -0300

    primeiro commit

---
+++ semaforo.js
@@ -0,0 +1,32 @@
+const img = document.getElementById( 'img'); // documento obter ElmentoPorID
+const buttons =  document.getElementById( 'buttons');
+let colorIndex= 0;
+let intervalid= null;
+
+const trafficLight= ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const nextIndex =() => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changeColor = () => {
+    const colors = ['red','yellow','green']
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic =() => {
+    clearInterval ( intervalid);
+
+}
+
+const turnOn = {
+    'red' : () => img.src ='./img/vermelho.png',
+    'yellow': () => img.src ='./img/amarelo.png',
+    'green' : () => img.src ='./img/verde.png',
+    'automatic': () => intervalid = setInterval( changeColor, 1000)
+}
+
+buttons.addEventListener('click', trafficLight);
